4b75846d6fc02b869a6e8e7b1a12cbf9870cfb7f
Ibrahim Berk Bulgan <berkbulgan@gmail.com>
2022-04-06 14:16:18 +0300

    fixed insta function launch

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const container = document.querySelector(".grid-container");
 const drawBtn = document.querySelector("#drawBtn");
-
+let gridBoxes = document.querySelectorAll(".grid-item")
 
 function drawGrid(size){
     console.log(size);
@@ -15,15 +15,20 @@ function drawGrid(size){
         container.appendChild(boxes[i]);
     }
 }
+function updateNodes(){
+    gridBoxes = document.querySelectorAll(".grid-item");
+    gridBoxes.forEach(box => box.addEventListener("click", paintBox));
 
+}
 function paintBox(e){
     this.style.backgroundColor = selectColor.value;
 }
 
 const selectSize = document.querySelector("#selectSize");
 const selectColor = document.querySelector("#selectColor");
-const gridBoxes = document.querySelectorAll(".grid-item");
-drawBtn.addEventListener('click', drawGrid(selectSize.value));
-gridBoxes.forEach(box => box.addEventListener('click', paintBox));
+drawBtn.addEventListener("click", function(){
+    drawGrid(selectSize.value);
+    updateNodes()});
+    
 
 
